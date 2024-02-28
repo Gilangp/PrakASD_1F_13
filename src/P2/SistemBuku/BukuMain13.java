756862c51b960156775f0fd7898f54eb1d17b920
Gilangp <gilangpurnomo0505@gmail.com>
2024-02-28 13:11:19 +0700

    P2_Percobaan2_Pertanyaan5_1F_13

--- src/P2/SistemBuku/BukuMain13.java
+++ src/P2/SistemBuku/BukuMain13.java
@@ -13,5 +13,13 @@ public class BukuMain13 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+
+        Buku13 bk2 = new Buku13();
+        bk2.judul = "How to Train Your Dragon";
+        bk2.pengarang = "Cressida Cowell";
+        bk2.halaman = 432;
+        bk2.stok = 0;
+        bk2.harga = 275000;
+        bk2.tampilInformasi();
     }
 }
