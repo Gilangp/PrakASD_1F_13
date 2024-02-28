8b731f27b002d9aa6809be0d3dfb4cfabe832b03
Gilangp <gilangpurnomo0505@gmail.com>
2024-02-29 00:02:49 +0700

    P2_Laporan Pratikum_1F_13

--- src/P2/GameDragon/DragonMain13.java
+++ src/P2/GameDragon/DragonMain13.java
@@ -2,7 +2,7 @@ package P2.GameDragon;
 
 public class DragonMain13 {
     public static void main(String[] args) {
-        Dragon13 dragon1  = new Dragon13(2,3,4,5);
+        Dragon13 dragon1  = new Dragon13(0,3,0,5);
         
         dragon1.moveLeft();
         dragon1.tampilanPosisi();
