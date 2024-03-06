dbc6d0e5d11af76096b108e4983b5f0086849ceb
Gilangp <gilangpurnomo0505@gmail.com>
2024-03-06 15:07:20 +0700

    P3_Percobaan3_Pertanyaan5_1F_13

--- src/P3/ArrayBalok/sgArray13.java
+++ src/P3/ArrayBalok/sgArray13.java
@@ -8,5 +8,9 @@ public class sgArray13 {
         sgArray[1] = new Segitiga13(20, 10);
         sgArray[2] = new Segitiga13(15, 6);
         sgArray[3] = new Segitiga13(25, 10);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Segitiga ke-" + i + " dengan Luas: " + sgArray[i].hitungLuas() + " dan Keliling: " + sgArray[i].hitungKeliling());
+        }
     }
 }
