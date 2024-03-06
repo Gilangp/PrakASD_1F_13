985d51a5c3f89c6095e4fb4a5a4d43a573ec464e
Gilangp <gilangpurnomo0505@gmail.com>
2024-03-06 14:31:21 +0700

    P3_Percobaan3_Pertanyaan1_1F_13

--- src/P3/ArrayBalok/Balok13.java
+++ src/P3/ArrayBalok/Balok13.java
@@ -5,10 +5,18 @@ public class Balok13 {
     public int lebar;
     public int tinggi;
 
+    // konstruktor berparameter
     public Balok13(int p, int l, int t) {
         panjang = p;
         lebar = l;
         tinggi = t;
+    }
+
+    // konstruktor tanpa parameter
+    public Balok13() {
+        panjang = 0;
+        lebar = 0;
+        tinggi = 0;
     }
 
     public int hitungVolume() {
