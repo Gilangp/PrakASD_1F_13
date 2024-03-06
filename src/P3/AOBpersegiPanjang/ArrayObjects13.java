d01eddd217fc0bb6454e4eb00a3d2fb97f3dc0de
Gilangp <gilangpurnomo0505@gmail.com>
2024-03-06 12:01:02 +0700

    P3_Percobaan1_1F_13

---
+++ src/P3/AOBpersegiPanjang/ArrayObjects13.java
@@ -0,0 +1,23 @@
+package P3.AOBpersegiPanjang;
+
+public class ArrayObjects13 {
+    public static void main(String[] args) {
+        PersegiPanjang13[] ppArray = new PersegiPanjang13[3];
+
+        ppArray[0] = new PersegiPanjang13();
+        ppArray[0].panjang = 110;
+        ppArray[0].lebar = 30;
+
+        ppArray[1] = new PersegiPanjang13();
+        ppArray[1].panjang = 80;
+        ppArray[1].lebar = 40;
+
+        ppArray[2] = new PersegiPanjang13();
+        ppArray[2].panjang = 100;
+        ppArray[2].lebar = 20;
+
+        System.out.println("Persegi Panjang ke-0, panjang: " + ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
+        System.out.println("Persegi Panjang ke-0, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
+        System.out.println("Persegi Panjang ke-0, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+    }
+}
---
+++ src/P3/AOBpersegiPanjang/PersegiPanjang13.java
@@ -0,0 +1,7 @@
+package P3.AOBpersegiPanjang;
+
+public class PersegiPanjang13 {
+    public int panjang;
+    public int lebar;
+    
+}
