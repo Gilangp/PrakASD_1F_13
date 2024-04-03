f38e433afdedfd261f8ed8a02ff8a9884c25b4d5
Gilangp <gilangpurnomo0505@gmail.com>
2024-04-03 11:57:04 +0700

    P7_Percobaan1_1F_13

---
+++ src/P7/Buku13.java
@@ -0,0 +1,25 @@
+package P7;
+
+public class Buku13 {
+    int kodeBuku;
+    String judulBuku;
+    int tahunTerbit;
+    String pengarang;
+    int stock;
+
+    public Buku13(int kodeBuku, String judulBuku, int tahunTerbit, String pengarang, int stock) {
+        this.kodeBuku = kodeBuku;
+        this.judulBuku = judulBuku;
+        this.tahunTerbit = tahunTerbit;
+        this.pengarang = pengarang;
+        this.stock = stock;
+    }
+
+    public void tampilDataBuku() {
+        System.out.println("Kode buku : " + kodeBuku);
+        System.out.println("Judul buku : " + judulBuku);
+        System.out.println("Tahun Terbit : " + tahunTerbit);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Stock : " + stock);
+    }
+}
---
+++ src/P7/PencarianBuku13.java
@@ -0,0 +1,51 @@
+package P7;
+
+public class PencarianBuku13 {
+    Buku13 listBk[] = new Buku13[5];
+    int idx;
+
+    void tambah(Buku13 m) {
+        if (idx < listBk.length) {
+            listBk[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (Buku13 m : listBk) {
+            m.tampilDataBuku();
+        }
+    }
+
+    public int FindSeqSearch(int cari) {
+        int posisi = -1;
+        for (int j = 0; j < listBk.length; j++) {
+            if (listBk[j].kodeBuku == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public void Tampilposisi(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("data : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+    public void TambahData(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Kode buku\t : " + x);
+            System.out.println("Judul buku\t : " + listBk[pos].judulBuku);
+            System.out.println("Tahun Terbit\t : " + listBk[pos].tahunTerbit);
+            System.out.println("Pengarang\t : " + listBk[pos].pengarang);
+            System.out.println("Stock\t\t : " + listBk[pos].stock);
+        } else {
+            System.out.println("data " + x + "tidak ditemukan");
+        }
+    }
+}
