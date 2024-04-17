64421758af5719d980c92b1bd16a0eb8793b3fce
Gilangp <gilangpurnomo0505@gmail.com>
2024-04-17 11:41:08 +0700

    P8_Percobaan1_1F_13

---
+++ src/P8/Gudang13.java
@@ -0,0 +1,75 @@
+package P8;
+
+public class Gudang13 {
+    Barang13[] tumpukan;
+    int size;
+    int top;
+
+    public Gudang13(int kapasitas) {
+        size = kapasitas;
+        tumpukan = new Barang13[size];
+        top = -1;
+    }
+
+    public boolean cekKosong() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cekPenuh() {
+        if (top == size -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void tambahBarang(Barang13 brg) {
+        if (!cekPenuh()) {
+            top++;
+            tumpukan[top] = brg;
+            System.out.println("Barang " + brg.nama + " berhasil ditambahkan Ke Gudang");
+        } else {
+            System.out.println("Gagal Tumpukan barang di Gudang sudah penuh");
+        }
+    }
+
+    public Barang13 ambilBarang() {
+        if (!cekKosong()) {
+            Barang13 delete = tumpukan[top];
+            top--;
+            System.out.println("Barang " + delete.nama + " diambil dari Gudang.");
+            return delete;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public Barang13 lihatBarangTeratas() {
+        if (!isEmpty) {
+            Barang13 barangTeratas =  tumpukan[top];
+            System.out.println("Barang teratas: " + barangTeratas.nama);
+            return barangTeratas;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public void tampilkanBarang() {
+        if (!cekKosong()) {
+            System.out.println("Rincian tumpukan barang di Gudang: ");
+
+            for (int i = 0; i <= top; i++) {
+                System.out.printf("Kode %d: %s (Kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama, 
+                tumpukan[i].kategori);
+            }
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+        }
+    }
+}
---
+++ src/P8/Utama13.java
@@ -0,0 +1,45 @@
+package P8;
+
+import java.util.Scanner;;
+
+public class Utama13 {
+    public static void main(String[] args) {
+        Gudang13 gudang = new Gudang13(7);
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nMenu");
+            System.out.println("1. Tambah barang");
+            System.out.println("2. Ambil barang");
+            System.out.println("3. Tampil tumpukan barang");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih operasi: ");
+            int pilihan = input.nextInt();
+            input.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan kode barang: ");
+                    int kode = input.nextInt();
+                    input.nextLine();
+                    System.out.print("Masukkan nama barang: ");
+                    String nama = input.nextLine();
+                    System.out.print("Masukkan nama kategori: ");
+                    String kategori = input.nextLine();
+                    Barang13 barangBaru = new Barang13(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+                case 2:
+                    gudang.ambilBarang();
+                    break;
+                case 3:
+                    gudang.tampilkanBarang();
+                case 4:
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silahkan coba lagi.");
+            }
+        }
+    }
+}
