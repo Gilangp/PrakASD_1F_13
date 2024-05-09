8f26f0ed769cbaa34270474d4b380f19740f986a
Gilangp <gilangpurnomo0505@gmail.com>
2024-05-09 11:57:38 +0700

    P10_Praktikum2_1F_13

---
+++ src/P10/Praktikum2/Nasabah13.java
@@ -0,0 +1,23 @@
+package P10.Praktikum2;
+
+public class Nasabah13 {
+    String norek;
+    String nama;
+    String alamat;
+    int umur;
+    double saldo;
+
+
+    public Nasabah13(String norek, String nama, String alamat, int umur, double saldo) {
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+    }
+
+
+    public Nasabah13() {
+
+    }
+}
