df5aa3db62db3fb3b420d014085bf0bc9560bf98
Gilangp <gilangpurnomo0505@gmail.com>
2024-05-15 12:40:01 +0700

    P11_Percobaan1_1F_13

---
+++ src/P11/SingleLinkedList/Node13.java
@@ -0,0 +1,12 @@
+package P11.SingleLinkedList;
+
+public class Node13 {
+    int data;
+    Node13 next;
+
+
+    public Node13(int nilai, Node13 berikutnya) {
+        data = nilai;
+        next = berikutnya;
+    }
+}
---
+++ src/P11/SingleLinkedList/SingleLinkedList13.java
@@ -0,0 +1,80 @@
+package P11.SingleLinkedList;
+
+public class SingleLinkedList13 {
+    Node13 head, tail;
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    void print() {
+        if (!isEmpty()) {
+            Node13 tmp = head;
+            System.out.print("Isi Linked List:  ");
+            while (tmp != null) {
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println("");
+        } else {
+            System.out.println("Linked List kosong");
+        }
+    }
+
+    void addFirst(int input) {
+        Node13 ndInput = new Node13(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+    void addLast(int input) {
+        Node13 ndInput = new Node13(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            tail.next = ndInput;
+            tail = ndInput;
+        }
+    }
+
+    void insertAfter(int key, int input) {
+        Node13 ndinput = new Node13(input, null);
+        Node13 temp = head;
+        do {
+            if(temp.data == key) {
+                ndinput.next = temp.next;
+                temp.next = ndinput;
+                if (ndinput.next == null) {
+                    tail = ndinput;
+                }
+                break;
+            }
+            temp = temp.next;
+        } while (temp != null);
+    }
+
+    void insertAt(int index, int input) {
+        Node13 ndInput = new Node13(input, null);
+        if (index < 0) {
+            System.out.println("perbaiki logikanya!"
+                    + "kalau indeksnya -1 bagaimana???");
+        } else if (index == 0) {
+            addFirst(input);
+        } else {
+            Node13 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = new Node13(input, temp.next);
+            if (temp.next.next == null) {
+                tail = temp.next;
+            }
+        }
+    }
+}
