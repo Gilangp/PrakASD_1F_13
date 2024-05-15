5b9cc887c8bbdf173d6d527f6844ba1fefef1c52
Gilangp <gilangpurnomo0505@gmail.com>
2024-05-15 15:23:27 +0700

    P11_Percobaan2_1F_13

--- src/P11/SingleLinkedList/SingleLinkedList13.java
+++ src/P11/SingleLinkedList/SingleLinkedList13.java
@@ -93,8 +93,8 @@ public class SingleLinkedList13 {
             tmp = tmp.next;
             index++;
         }
-        if (tmp != null) {
-            return -1;
+        if (tmp == null) {
+            return 1;
         } else {
             return index;
         }
