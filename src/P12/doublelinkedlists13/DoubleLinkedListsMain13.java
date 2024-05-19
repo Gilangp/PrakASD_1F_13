2b21938f734a8983463d54b26f045e2d08814af2
Gilangp <gilangpurnomo0505@gmail.com>
2024-05-19 10:06:52 +0700

    P12_Percobaan2_1F_13

--- src/P12/doublelinkedlists13/DoubleLinkedListsMain13.java
+++ src/P12/doublelinkedlists13/DoubleLinkedListsMain13.java
@@ -3,22 +3,23 @@ package P12.doublelinkedlists13;
 public class DoubleLinkedListsMain13 {
     public static void main(String[] args) throws Exception {
         DoubleLinkedLists13 dll = new DoubleLinkedLists13();
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
         dll.print();
         System.out.println("Size : " + dll.size());
         System.out.println("========================================");
-        dll.addFirst(3);
-        dll.addLast(4);
-        dll.addFirst(7);
+        dll.removeFirst();
         dll.print();
         System.out.println("Size : " + dll.size());
         System.out.println("========================================");
-        dll.add(40, 1);
+        dll.removeLast();
         dll.print();
         System.out.println("Size : " + dll.size());
         System.out.println("========================================");
-        dll.clear();
+        dll.remove(1);
         dll.print();
         System.out.println("Size : " + dll.size());
-        System.out.println("========================================");
     }
 }
