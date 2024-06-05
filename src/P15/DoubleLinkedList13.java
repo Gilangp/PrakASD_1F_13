a2476c535d0482b0e8050bcb63541aae6b8dbb9a
Gilangp <gilangpurnomo0505@gmail.com>
2024-06-05 11:53:56 +0700

    P15_Percobaan1_1F_13

---
+++ src/P15/DoubleLinkedList13.java
@@ -0,0 +1,75 @@
+package P15;
+
+public class DoubleLinkedList13 {
+    Node13 head;
+    int size;
+
+    public DoubleLinkedList13() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas.");
+        }
+        Node13 current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+    
+    public void addFirst(int item, int jarak) {
+        if (isEmpty()) {
+            head = new Node13(null, item, jarak, null);
+        } else {
+            Node13 newNode = new Node13(null, item, jarak, head);
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public int getJarak(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai ideks di luar batas");
+        }
+        Node13 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.jarak;
+    }
+
+    public void remove(int index) {
+        Node13 current = head;
+        while (current != null) {
+            if (current.data == index) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                }
+                break;
+            }
+            current = current.next;
+        }
+    }
+}
